e347fd9a50cde427c7ab5f0556e58c22fa755ff1
Samuel <samuel.forner@yahoo.fr>
2023-12-04 18:58:17 -0500

    feat: working on first filter numberofroom only design & some logic

--- src/components/dashboard/Home/researchBar.tsx
+++ src/components/dashboard/Home/researchBar.tsx
@@ -1,6 +1,31 @@
-import { Input, Stack } from '@chakra-ui/react';
+import React from 'react';
+
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+  Button,
+  HStack,
+  Input,
+  Menu,
+  MenuButton,
+  MenuList,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+} from '@chakra-ui/react';
 
 export function ResearchBar() {
+  // liste de filtre : nombre de chambre/superficie/adresse : rue;ville;pays
+  const [roomNumber, setRoomNumber] = React.useState<
+    number | string | undefined
+  >(0);
+
+  const handleRoomNumber = (value: number | string) => {
+    setRoomNumber(value);
+  };
+
   return (
     <Stack spacing={4} padding={4}>
       <Input
@@ -10,6 +35,30 @@ export function ResearchBar() {
         borderColor='gray.300'
         padding={2}
       />
+      <HStack>
+        <Stack>
+          <Menu closeOnSelect={false} autoSelect={false}>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              {roomNumber
+                ? `Nombre de chambre : ${roomNumber}`
+                : 'Nombre de chambre'}
+            </MenuButton>
+            <MenuList>
+              <NumberInput
+                size='xs'
+                value={roomNumber}
+                onChange={handleRoomNumber}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </MenuList>
+          </Menu>
+        </Stack>
+      </HStack>
     </Stack>
   );
 }
